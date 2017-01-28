312f587a0535222432edf30cc6e0cb600b46e18e
jheckjr <joel.heckjr@gmail.com>
2017-01-28 12:47:43 -0500

    test new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 
+import { RecipeListComponent } from './recipe-list/recipe-list.component';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
